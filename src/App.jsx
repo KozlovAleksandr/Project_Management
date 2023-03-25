4b2b57bc76d24da102f89c5ea579165e610599b5
KozlovAleksandr <aleksandr.an.kozlov@gmail.com>
2023-03-25 19:52:30 +0300

    Заглушки для компонентов header , content , footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Header from './components/Header/Header';
+import Content from './components/Content/Content';
+import Footer from './components/Footer/Footer';
+
 import './App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
